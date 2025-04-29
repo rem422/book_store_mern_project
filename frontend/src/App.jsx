0597d582c619ca80d1abc6f3656defe74599ad9c
Rem Simiyu <remsimiyu@gmail.com>
2025-04-29 12:17:27 +0300

    Book Store MERN Project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <main className='min-h-screen'>
+      <main className='min-h-screen max-w-screen-2xl mx-auto'>
         <Outlet />
       </main>
       <Footer />
