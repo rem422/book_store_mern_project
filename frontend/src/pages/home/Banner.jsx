4f896615c11d7d75e13d5f591a04b74881ea732e
Rem Simiyu <remsimiyu@gmail.com>
2025-05-13 23:37:30 +0300

    Book Store MERN Project

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
         </div>
         <div className='md:w-1/2 w-full'>
             <h1 className='md:text-5xl text-2xl font-medium mb-7'>New Releases This Week</h1>
-            <p className='mb-10'>It's time to update your reading list with some of the latest and greatest releases in the literary world. From heart-pumping thrillers to captivating memoirs, this week's new releases offer something for everyone</p>
+            <p className='mb-10'>It's time to update your reading list with some of the latest and greatest releases in the literary world. From heart-pumping thrillers to captivating memoirs, this week's new releases offer something for everyone.</p>
             <button className='btn-primary'>Subscribe</button>
         </div>
     </div>
